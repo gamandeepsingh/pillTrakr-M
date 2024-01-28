5ca9ed8ad1993e8144962e5132d6dfc32e8b213c
gamandeepsingh <gamandeepsingh4@gmail.com>
2024-01-29 01:12:49 +0530

    commit

---
+++ frontend/src/screens/AdminScreen.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+
+
+function AdminScreen() {
+    return (
+        <div>
+            <div className="content-wrapper">
+                <div className="container-fluid">
+
+                    <div className="card mb-3">
+
+                        <div className="card-body">
+                            <div className="table-responsive">
+                                <table className="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                    <thead>
+                                        <tr>
+                                            <th>Contact Id</th>
+                                            <th>Full Name</th>
+                                            <th>Email</th>
+                                            <th>Message</th>
+                                            <th>City</th>
+                                            <th>Start date</th>
+                                        </tr>
+                                    </thead>
+
+                                    <tbody>
+                                        <tr>
+                                            
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    )
+}
+
+export default AdminScreen
